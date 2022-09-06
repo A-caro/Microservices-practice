4cfa7511297d6447eddb8d7e629ce5b30665e97c
A-caro <carolinanatampis@gmail.com>
2022-09-06 20:00:08 -0300

    update

--- market/src/main/java/com/example/market/service/MarketService.java
+++ market/src/main/java/com/example/market/service/MarketService.java
@@ -1,7 +1,21 @@
 package com.example.market.service;
 
+import com.example.market.entity.Market;
+import com.example.market.repository.MarketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MarketService {
+
+    @Autowired
+    MarketRepository marketRepository;
+
+    public List<Market> getAll(){
+        return marketRepository.findAll();
+    }
+
+
 }
