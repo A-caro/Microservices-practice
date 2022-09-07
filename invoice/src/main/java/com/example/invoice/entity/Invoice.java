4454416034debb1f9c49fa72745b4e8c2cee89af
A-caro <carolinanatampis@gmail.com>
2022-09-07 15:55:35 -0300

    update microservice II

--- invoice/src/main/java/com/example/invoice/entity/Invoice.java
+++ invoice/src/main/java/com/example/invoice/entity/Invoice.java
@@ -1,2 +1,24 @@
-package com.example.invoice.entity;public class Invoice {
+package com.example.invoice.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String number;
+    private String article;
+    private Long clientId;
 }
